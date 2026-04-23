b8f748e412d82e9c86bafeb2e2750d24daf08081
Rohit <khatrirohit2002@gmail.com>
2026-04-23 10:37:47 +0530

    signin functionality complete

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { upload } from "../middlewares/multer.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", upload.single("profilePicture"), asyncHandler(signup));
+router.post("/signin", asyncHandler(signin));
 
 export default router;
