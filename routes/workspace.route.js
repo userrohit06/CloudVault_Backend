4e0aa9f249ded1f86ed9e65d67fd6025f65259dd
Rohit <khatrirohit2002@gmail.com>
2026-04-26 11:04:51 +0530

    workspace features donoe

--- routes/workspace.route.js
+++ routes/workspace.route.js
@@ -3,8 +3,10 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 import {
   acceptInvite,
   createWorkspace,
+  deleteWorkspace,
   inviteMember,
   leaveWorkspace,
+  rejectInvite,
 } from "../controllers/workspace.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -13,4 +15,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createWorkspace);
 router.post("/invite", verifyToken, asyncHandler(inviteMember));
 router.post("/accpet", verifyToken, acceptInvite);
+router.post("/reject", verifyToken, asyncHandler(rejectInvite));
 router.post("/leave", verifyToken, leaveWorkspace);
+router.delete("/remove", verifyToken, deleteWorkspace);
